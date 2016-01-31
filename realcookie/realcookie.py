cdb19950f0f79692f71eb1f87ffb91cdc6855ecd
secknv <secknv@gmail.com>
2016-01-31 04:01:03 +0000

    version 1.0.2

--- realcookie/realcookie.py
+++ realcookie/realcookie.py
@@ -14,6 +14,7 @@ class cookie:
 
     # returns the path to a random cookie in the cookies folder
     def gib(self):
+        self.img_number = len(os.listdir(self.folder))
         a = random.randint(1, self.img_number + 1)
         path = self.folder + '/' + str(a) + '.png'
         return path
@@ -27,11 +28,15 @@ class cookie:
         except IndexError:
             new_name = '1.png'
         urllib.request.urlretrieve(img_url, self.folder + '/' + new_name)
+        self.img_number = len(os.listdir(self.folder))
 
     # returns the number of cookie images in the folder
     def count(self):
+        self.img_number = len(os.listdir(self.folder))
         return self.img_number
 
     # deletes all the images and the folder. usefull for resets
     def rm(self):
         shutil.rmtree(self.folder)
+        os.makedirs(self.folder)
+        self.img_number = len(os.listdir(self.folder))
