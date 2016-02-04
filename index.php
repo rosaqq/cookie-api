284812766530098d8ededd1de851fea9ae8ba5f6
TheEvilSocks <theevilsocks@gmail.com>
2016-02-04 18:16:41 +0100

    Update
    When adding the parameter '?image' it will show the actual image (with
    the right headers) instead of the url.

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 $cookieLinks = file('cookieurls.txt');
-echo $cookieLinks[array_rand($cookieLinks)];
+$theChosenOne = $cookieLinks[array_rand($cookieLinks)];
+
+	if(isset($_GET['image'])){
+		switch(strtolower(substr(strrchr(basename($theChosenOne),"."),1))) {
+		    case "gif": $ctype="image/gif"; break;
+		    case "png": $ctype="image/png"; break;
+		    case "jpeg":$ctype="image/jpeg"; break;
+		    case "jpg": $ctype="image/jpeg"; break;
+		    default:
+		}
+
+		header('Content-type: ' . $ctype);
+		readfile($theChosenOne);
+	}else{
+		echo $theChosenOne;
+	}
 ?>
