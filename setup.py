376fd55b29a85b70a43b2c2eb046983aa64e9197
secknv <secknv@gmail.com>
2016-02-21 01:21:17 +0000

    nothing special

--- setup.py
+++ setup.py
@@ -5,12 +5,19 @@ def readme():
         return f.read()
 
 setup(name='realcookie',
-      version='1.0.5',
+      version='1.0.6',
       description="Benolot's cookie API, python version",
       long_description='This module allows you to save cookie images from a URL to a folder and then provides you with the path to a random image in that folder.',
       url='https://github.com/secknv/cookie-api',
       author='secknv',
       license='MIT',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
       packages=['realcookie'],
       include_package_data=True,
       zip_safe=False)
